0ac1ba3cc444d1d10e00bd9470b1e92a7ef8fa9f
rajyan <38206553+rajyan@users.noreply.github.com>
2019-07-23 08:47:13 +0900

    fixed template

--- library/template.cpp
+++ library/template.cpp
@@ -13,7 +13,7 @@ const int MOD = 1000000007, INF = 1111111111;
 using namespace std;
 using lint = long long;
 
-template <typename T>
+template <class T>
 ostream &operator<<(ostream &os, const vector<T> &vec) {
 	for (int i = 0; i < vec.size(); i++) {
 		os << vec[i] << (i + 1 == vec.size() ? "" : " ");
@@ -31,7 +31,7 @@ void dump(const char* str, Head &&h, Tail &&... t) {
 }
 #define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
 #else 
-#define DMP(x) ((void)0)
+#define DMP(...) ((void)0)
 #endif
 
 int main() {
