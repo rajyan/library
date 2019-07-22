e00ffb38bf3e7507563830dababd0624e85e5621
rajyan <38206553+rajyan@users.noreply.github.com>
2019-07-23 05:35:26 +0900

    fixed CumulativeSum, added RectangularSum, fixed vector 2d, added coutyesno

---
+++ library/CumulativeSum.cpp
@@ -0,0 +1,59 @@
+#include<vector>
+
+template <class T>
+class CuSum {
+private:
+	vector<T> cusum;
+	int len = 0;
+public:
+	// Constructors 
+	CuSum(vector<T> const &vec) : len(vec.size()), cusum(len + 1) {
+		for (int i = 0; i < len; i++) cusum[i + 1] = cusum[i] + vec[i];
+	}
+	CuSum(int N) : len(N), cusum(N + 1) {}
+	void add(int i, T x) { cusum[i + 1] += x; }
+	void init() { for (int i = 0; i < len; i++) cusum[i + 1] += cusum[i]; }
+
+	// partial sum of 0-indexed [l, r] 
+	T operator()(int l, int r) {
+		if (r < l || r < 0) {
+			assert(0);
+			return T(0);
+		}
+		else return cusum[r + 1] - cusum[l];
+	}
+	// 0-indexed
+	T operator[](int i) { return cusum[i + 1]; }
+	T back() { return cusum.back(); }
+};
+
+// 2d Cumulative Sum
+template< class T >
+class RecSum {
+private:
+	vector<vector<T>> data;
+	int H = 0, W = 0;
+public:
+	RecSum(vector<vector<T>> const &vec) : H(vec.size()), W(vec[0].size()), data(H + 1, vector<T>(W + 1, 0)) {
+		for (int row = 0; row <= H; row++) {
+			for (int col = 0; col <= W; col++) {
+				data[row + 1][col + 1] += data[row + 1][col] + data[row][col + 1] - data[row][col] + vec[row][col];
+			}
+		}
+	}
+
+	RecSum(int H, int W) : H(H), W(W), data(H + 1, vector<T>(W + 1, 0)) {}
+	void add(int x, int y, T z) { data[x + 1][y + 1] += z; }
+	void init() {
+		for (int row = 0; row < H; row++) {
+			for (int col = 0; col < W; col++) {
+				data[row + 1][col + 1] += data[row + 1][col] + data[row][col + 1] - data[row][col];
+			}
+		}
+	}
+
+	// [sx, gx) x [sy, gy)
+	T operator()(int sx, int sy, int gx, int gy) {
+		return data[gx][gy] - data[sx][gy] - data[gx][sy] + data[sx][sy];
+	}
+};
