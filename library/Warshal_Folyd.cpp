a2349f22b0792cee46acce4923cf2a6337898995
kitakita7617 <kitakita7617@gmail.com>
2019-08-20 10:04:53 +0900

    fixed warshal_floyd, updated edge

--- library/Warshal_Folyd.cpp
+++ library/Warshal_Folyd.cpp
@@ -5,23 +5,6 @@
 using namespace std;
 const int INF = 1111111111;
 
-template<class T>
-vector<T> Warshal_Floyd(int N, vector<T> &d) {
-
-	for (int k = 0; k < N; k++) {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-
-				if (d[i][k] != INF && d[k][j] != INF) {
-					d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
-				}
-			}
-		}
-	}
-	return d;
-}
-
-////example Input
 //vector<vector<int>> d(N, vector<int>(N, INF));
 //for (int i = 0; i < N; i++) d[i][i] = 0;
 //
@@ -32,3 +15,18 @@ vector<T> Warshal_Floyd(int N, vector<T> &d) {
 //	d[a][b] = c;
 //	d[b][a] = c;
 //}
+
+auto Warshal_Floyd = [&](int N, vector<vector<int>> &d) {
+
+	for (int k = 0; k < N; k++) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (d[i][k] != INF && d[k][j] != INF) {
+					d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+				}
+			}
+		}
+	}
+	return d;
+}
+
