209a7b7717cb1b03e225aa1d21a4634c3fe510b4
kitakita7617 <kitakita7617@gmail.com>
2019-10-21 22:29:29 +0900

    add compress, fixed slide_minmax

---
+++ library/Compress.cpp
@@ -0,0 +1,27 @@
+template<class T>
+class Compress {
+private:
+	vector<T> comp;
+
+public:
+	Compress() {}
+	Compress(const vector< T > &vec) : comp(vec) { init(); }
+
+	void add(const T &x) { comp.emplace_back(x); }
+
+	void init() {
+		sort(begin(comp), end(comp));
+		comp.erase(unique(begin(comp), end(comp)), end(comp));
+	}
+
+	vector<int> get(const vector<T> &vec) const {
+		vector<int> ret(vec.size());
+		transform(begin(vec), end(vec), ret.begin(), [&](const T &x) {
+			return (int)(lower_bound(begin(comp), end(comp), x) - begin(comp));
+		});
+		return ret;
+	}
+	int get(const T &x) const { return lower_bound(begin(comp), end(comp), x) - begin(comp); }
+
+	const T &operator[](int k) const { return comp[k]; }
+};
