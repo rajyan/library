4decd5b5d4b200363e9b5f5d1ac5c040897b45ab
kitakita7617 <kitakita7617@gmail.com>
2019-11-11 10:15:35 +0900

    fixed recsum

--- library/Cumulative_sum.cpp
+++ library/Cumulative_sum.cpp
@@ -27,17 +27,16 @@ public:
 };
 
 // 2d Cumulative Sum
-template<class T>
+template< class T >
 class RecSum {
 private:
-	vector<vector<T>> data;
 	int H, W;
+	vector<vector<T>> data;
 
 public:
-	RecSum(vector<vector<T>> const &vec) : H(vec.size()), W(vec[0].size()) {
-		data.resize(H + 1, vector<T>(W + 1, 0));
-		for (int row = 0; row <= H; row++) {
-			for (int col = 0; col <= W; col++) {
+	RecSum(vector<vector<T>> const &vec) : H(vec.size()), W(vec[0].size()), data(H + 1, vector<T>(W + 1, 0)) {
+		for (int row = 0; row < H; row++) {
+			for (int col = 0; col < W; col++) {
 				data[row + 1][col + 1] += data[row + 1][col] + data[row][col + 1] - data[row][col] + vec[row][col];
 			}
 		}
@@ -45,7 +44,7 @@ public:
 
 	RecSum(int H, int W) : H(H), W(W), data(H + 1, vector<T>(W + 1, 0)) {}
 	void add(int x, int y, T z) { data[x + 1][y + 1] += z; }
-	void build() {
+	void init() {
 		for (int row = 0; row < H; row++) {
 			for (int col = 0; col < W; col++) {
 				data[row + 1][col + 1] += data[row + 1][col] + data[row][col + 1] - data[row][col];
