e8af9ae162e6621646c8b0f16e61a0a8fac08c59
kitakita7617 <kitakita7617@gmail.com>
2019-12-29 06:34:46 +0900

    fixed binary_search

--- library/binary_search.cpp
+++ library/binary_search.cpp
@@ -1,15 +1,15 @@
 
-auto judge = [&](auto idx, auto key) {
-	return idx >= key;
+auto judge = [&](auto idx) {
+	return idx;
 };
 
-auto binary_search = [&](auto f, auto key) {
+auto binary_search = [&](auto f) {
 	
 	auto ng = -1;
 	auto ok = ;
 
 	while (abs(ok - ng) > 1) {
-		int mid = (ok + ng) / 2;
+		auto mid = (ok + ng) / 2;
 
 		if (f(mid, key)) ok = mid;
 		else ng = mid;
