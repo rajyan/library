c2797db6c613103e0e2f9d2e1fb924149311a59f
rajyan <kitakita7617@gmail.com>
2020-07-25 13:23:54 +0900

    reordered includes to apply _CRT_SECURE_NO_WARNINGS correctly

--- library/template.cpp
+++ library/template.cpp
@@ -1,3 +1,11 @@
+
+#ifdef _DEBUG
+#include "../../library/library/debug_template.cpp"
+#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
+#else 
+#define DMP(...) ((void)0)
+#endif
+
 #include <cassert>
 #include <cstdio>
 #include <cmath>
@@ -7,21 +15,16 @@
 #include <vector>
 #include <set>
 #include <map>
+#include <unordered_map>
 #include <queue>
 #include <numeric>
 #include <algorithm>
+#include <bitset>
 
 using namespace std;
 using lint = long long;
 constexpr int MOD = 1000000007, INF = 1010101010;
 constexpr lint LINF = 1LL << 60;
-
-#ifdef _DEBUG
-#include "../..library/library/debug_template.cpp"
-#define DMP(...) dump(#__VA_ARGS__, __VA_ARGS__)
-#else 
-#define DMP(...) ((void)0)
-#endif
 
 struct init {
 	init() {
