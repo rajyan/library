c77d76b2a191bf59bc4ef36f3ce16343bddf7993
rajyan <kitakita7617@gmail.com>
2020-08-14 21:35:23 +0900

    modified to 0-based and added method set

--- library/FenwickTree.cpp
+++ library/FenwickTree.cpp
@@ -2,21 +2,25 @@
 
 using namespace std;
 
-template<typename T = lint>
+template<class T>
 class FenwickTree {
 private:
-	vector<T> data;
-
+	int n;
+	vector<T> bit;
 public:
-	FenwickTree(int sz) : data(sz + 1) {}
+	FenwickTree(int sz, T&& x = T(0)) : n(sz), bit(n + 1) {
+		for (int i = 0; i < n; i++) add(i, x);
+	}
 
-	T sum(int k) {
+	T sum(int k) const {
 		T res = 0;
-		for (++k; k > 0; k -= k & -k) res += data[k];
+		for (k = k - 1; k >= 0; k = (k & (k + 1)) - 1) res += bit[k];
 		return res;
 	}
+	T sum(int l, int r) const { return sum(r) - sum(l); }
 
-	void add(int k, T x) {
-		for (++k; k < data.size(); k += k & -k) data[k] += x;
+	void add(int k, const T& x) {
+		for (; k < n; k |= k + 1) bit[k] += x;
 	}
+	void set(int k, const T& x) { add(k, x - sum(k, k + 1)); }
 };
