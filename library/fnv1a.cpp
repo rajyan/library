2b8fa95048788087eca2b8ab38c41c4221a615f2
rajyan <kitakita7617@gmail.com>
2020-08-23 11:44:21 +0900

    changed return type

--- library/fnv1a.cpp
+++ library/fnv1a.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
-#include <sstream>
 
 using namespace std;
 
-inline string fnv1a_64(string data) {
+inline uint64_t fnv1a_64(string data) {
 
 	uint64_t hash = 0xcbf29ce484222325;
-	uint64_t prime = 0x100000001b3;
+	constexpr uint64_t prime = 0x100000001b3;
 
 	for (int i = 0; i < (int)data.size(); ++i) {
 		hash = hash ^ data[i];
 		hash *= prime;
 	}
 
-	std::stringstream to_hex;
-	to_hex << std::hex << hash;
-
-	return to_hex.str();
-
+	return hash;
 }
