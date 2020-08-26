6783d1e5fa4608991f0924e8d9b23778f5faa12b
rajyan <kitakita7617@gmail.com>
2020-08-26 09:58:06 +0900

    added uniq_vec

--- library/RandNum.cpp
+++ library/RandNum.cpp
@@ -1,5 +1,7 @@
 #include <random>
 #include <chrono>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 using lint = long long;
@@ -9,11 +11,36 @@ struct RandNum {
 	mt19937 mt;
 	RandNum() : mt(chrono::steady_clock::now().time_since_epoch().count()) {}
 
-	lint operator()(lint a, lint b) {
-		uniform_int_distribution<lint> dist(a, b);
+	lint operator() (const lint& rand_min, const lint& rand_max) {
+		uniform_int_distribution<lint> dist(rand_min, rand_max);
 		return dist(mt);
 	}
-	lint operator() (lint b) { return (*this)(0LL, b); }
+	lint operator() (const lint &rand_max) { return (*this)(0LL, rand_max); }
+
+	vector<lint> uniq_vec(const int& sz, const lint& rand_min, lint rand_max) {
+
+		vector<lint> res(sz);
+		unordered_map<lint, lint> memo;
+		for (int i = 0; i < sz; i++, rand_max--) {
+
+			lint rand_val = (*this)(rand_min, rand_max);
+
+			// If rand_max hasn't been replaced yet, fill it with rand_max
+			if (memo.find(rand_max) == memo.end()) memo[rand_max] = rand_max;
+
+			auto val_itr = memo.find(rand_val);
+			if (val_itr == memo.end()) { // replace rand_val with rand_max
+				memo[rand_val] = memo[rand_max];
+			}
+			else { // If rand_val has already been replaced
+				rand_val = val_itr->second;
+				val_itr->second = memo[rand_max];
+			}
+
+			res[i] = rand_val;
+		}
+		return res;
+	}
 
 	template<class Ite>
 	void shuf(Ite first, Ite last) { shuffle(first, last, mt); }
