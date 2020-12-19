172a5911bf2ac048a75426b0b877c20e95dcac33
rajyan <kitakita7617@gmail.com>
2020-12-19 22:18:43 +0900

    fixed warnings

--- autosnippet/autosnippet.sh
+++ autosnippet/autosnippet.sh
@@ -5,10 +5,10 @@ template="snippet/auto_template.snippet"
 for cppfile in "$@"
 do	
 	if echo "$cppfile" | grep -q template; then
-		content=$(sed  -e '/[^\r\n]/,$!d' -e 's/[\&/]/\\&/g' -e 's/$/\\n/' $cppfile | tr -d '\n')
+		content=$(sed  -e '/[^\r\n]/,$!d' -e 's/[\&/]/\\&/g' -e 's/$/\\n/' "$cppfile" | tr -d '\n')
 	else
 		content=$(sed -e '/^#include/d' -e '/^using\ [nl]/d' -e '/^constexpr/d' -e '/[^\r\n]/,$!d' \
-			      -e 's/[\&/]/\\&/g' -e 's/$/\\n/' $cppfile | tr -d '\n')
+			      -e 's/[\&/]/\\&/g' -e 's/$/\\n/' "$cppfile" | tr -d '\n')
 	fi
 	
 	filename=${cppfile%.cpp}
