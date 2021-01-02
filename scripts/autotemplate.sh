a268d2ac7ac240b5f85ecf55f341dfd5d005adb0
rajyan <kitakita7617@gmail.com>
2021-01-02 17:57:47 +0900

    fixed indent problem and changed remormat to false

--- scripts/autotemplate.sh
+++ scripts/autotemplate.sh
@@ -6,9 +6,10 @@ config_dir=${CONFIG_DIR:-'/mnt/c/Users/kitak/AppData/Roaming/JetBrains/CLion2020
 config_file=${CONFIG_DIR:-'C_C__.xml'}
 
 for cppfile in "$@"; do
-  if [[ $cppfile = "*template.cpp" ]]; then
+  if [[ $cppfile == *'template.cpp' ]]; then
     # delete empty lines until content, replace end of line with EOF, drop \r or \n
     content=$(sed -e '/[^\r\n]/,$!d; s/$/EOL/' "$cppfile" | tr -d '\r\n')
+    echo "$cppfile"
   else
     # delete line starting with (#include | using (namespace|lint) | constexpr)
     content=$(sed -e '/^#include/d; /^using\ [nl]/d; /^constexpr/d' -e '/[^\r\n]/,$!d; s/$/EOL/' "$cppfile" | tr -d '\r\n')
@@ -21,15 +22,19 @@ for cppfile in "$@"; do
   shortname=${filename/_/}
   shortname=${shortname,,}
 
-  NAME="my${shortname}" VALUE=${content} envsubst < "$template" >"$temp_dir/$filename.xml"
+  NAME="my${shortname}" VALUE=${content} envsubst < "$template" > "$temp_dir/$filename.xml"
 done
 
+# generate templateSet
 templates='<templateSet group="C/C++">'
 for xml in "$temp_dir"/*; do
   [[ -e $xml ]] || break
-  [[ $xml =~ $config_file || $xml = "$template" ]] && continue
+  if [[ $xml == *"$config_file" || $xml == *"${template##*/}" ]]; then
+    echo "$xml"
+    continue
+  fi
   templates+=$(cat "$xml")
 done
 templates+='</templateSet>'
-echo $templates > "$temp_dir"/"$config_file"
+echo "$templates" > "$temp_dir"/"$config_file"
 cp -v "$temp_dir"/"$config_file" "$config_dir"/"$config_file"
