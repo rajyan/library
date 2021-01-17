902ab540c3732c339240f2d99169eb378e95e1b5
rajyan <kitakita7617@gmail.com>
2021-01-18 08:59:48 +0900

    fixed Edge

--- src/Edge.cpp
+++ src/Edge.cpp
@@ -3,5 +3,7 @@ struct Edge {
     int from{}, to{};
     T cost;
     Edge() = default;
+    Edge(int to, T cost) : to(to), cost(cost) {}
     Edge(int from, int to, T cost) : from(from), to(to), cost(cost) {}
+    bool operator>(const Edge &r) const { return this->cost > r.cost; }
 };
