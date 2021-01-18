8677f5a0b9edfbd19f6a83a16ecec5c302dda3b9
rajyan <kitakita7617@gmail.com>
2021-01-18 15:49:39 +0900

    fixed FloydWarshall and add tests

--- src/FloydWarshall.cpp
+++ src/FloydWarshall.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <vector>
 
 using namespace std;
@@ -6,15 +7,17 @@ using namespace std;
 
 template<class T>
 vector<vector<T>> FloydWarshall(const vector<vector<T>> &in, const T diag = T{}) {
+
     const int N = in.size();
-    assert(N == in[0].size());
+    assert(N == (int)in[0].size());
+    const T inf = in[0][0];
 
     auto d = in;
     for (int i = 0; i < N; i++) d[i][i] = diag;
     for (int k = 0; k < N; k++) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                chmin(d[i][j], d[i][k] + d[k][j]);
+                if (d[i][k] < inf && d[k][j] < inf) chmin(d[i][j], d[i][k] + d[k][j]);
             }
         }
     }
---
+++ test/aoj/GRL_1_C.test.cpp
@@ -0,0 +1,53 @@
+
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_C"
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+
+using namespace std;
+using lint = long long;
+constexpr lint LINF = 1LL << 60;
+
+#include "../../src/FloydWarshall.cpp"
+
+struct init {
+    init() {
+        cin.tie(nullptr);
+        ios::sync_with_stdio(false);
+        cout << fixed << setprecision(10);
+    }
+} init_;
+
+int main() {
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<vector<lint>> edges(N, vector<lint>(N, LINF));
+    for (int i = 0; i < M; i++) {
+        int u, v, c;
+        cin >> u >> v >> c;
+        edges[u][v] = c;
+    }
+
+    auto cost = FloydWarshall(edges);
+
+    for (int i = 0; i < N; i++) {
+        if (cost[i][i] < 0) {
+            cout << "NEGATIVE CYCLE\n";
+            return 0;
+        }
+    }
+
+    for (const auto &row : cost) {
+        for (const auto &e : row) {
+            cout << (e != LINF ? to_string(e) : "INF");
+            if (&e != &row.back()) cout << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
