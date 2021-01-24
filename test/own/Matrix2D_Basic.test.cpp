1bd7c3483a56ea99b0514458d0d73a443ffd856f
rajyan <kitakita7617@gmail.com>
2021-01-24 15:46:03 +0900

    fix test problem name

--- test/own/Matrix2D_Basic.test.cpp
+++ test/own/Matrix2D_Basic.test.cpp
@@ -1,5 +1,5 @@
 
-#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITM1_1_A"
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
 
 #include "../../src/makevec.cpp"
 #include "../../src/Matrix2D.cpp"
