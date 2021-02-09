1e83d11ae5d6bcf95246842b4400840453911a96
rajyan <kitakita7617@gmail.com>
2021-02-09 21:29:20 +0900

    add tests for MillerRabin

---
+++ test/aoj/ALDS1_1_C_2.test.cpp
@@ -0,0 +1,33 @@
+
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_1_C"
+
+#include "../../src/Prime.hpp"
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+
+using namespace std;
+using lint = long long;
+
+struct init {
+    init() {
+        cin.tie(nullptr);
+        ios::sync_with_stdio(false);
+        cout << fixed << setprecision(10);
+    }
+} init_;
+
+int main() {
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    // test Miller Rabin
+    Prime p(0);
+    cout << count_if(a.begin(), a.end(), [&p](auto &e) { return p.isPrime(e); }) << '\n';
+
+    return 0;
+}
