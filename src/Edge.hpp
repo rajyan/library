4471e9466d91261860a76ab08cd9156c66c81f73
rajyan <kitakita7617@gmail.com>
2021-02-22 08:22:40 +0900

    modified Edge

--- src/Edge.hpp
+++ src/Edge.hpp
@@ -5,7 +5,7 @@ struct Edge {
     int from{}, to{};
     T cost;
     Edge() = default;
-    Edge(int to, T cost) : to(to), cost(cost) {}
-    Edge(int from, int to, T cost) : from(from), to(to), cost(cost) {}
+    Edge(const int &to, const T &cost) : to(to), cost(cost) {}
+    Edge(const int &from, const int &to, const T &cost) : from(from), to(to), cost(cost) {}
     bool operator>(const Edge &r) const { return this->cost > r.cost; }
 };
