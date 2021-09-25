a14551a7fb5d4f38218353306284b940cc725669
Yohta Kimura <kitakita7617@gmail.com>
2021-09-26 08:39:17 +0900

    workflow testing

---
+++ src/test.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vector>
+
+using namespace std;
+
+class UnionFind {
+public:
+    explicit UnionFind(int size) : data(size, -1) {}
+    [[nodiscard]] int root(int x) { return data[x] < 0 ? x : data[x] = root(data[x]); }
+    [[nodiscard]] bool is_same(int x, int y) { return root(x) == root(y); }
+    [[nodiscard]] int size(int x) { return -data[root(x)]; }
+
+    bool unify(int x, int y) {
+        x = root(x);
+        y = root(y);
+        if (x != y) {
+            if (data[y] < data[x]) swap(x, y);
+            data[x] += data[y];
+            data[y] = x;
+            return true;
+        }
+        return false;
+    }
+
+private:
+    vector<int> data;
+};
